eef3598c3fbc62d639223a38af2c3f72eaae7a49
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-08-31 19:48:58 -0700

    going to push go to its limits

--- main.go
+++ main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+type Person struct {
+	firstname string
+	lastName  string
+	age       int
+	weight    int
+}
+
+// create a new person!!!
+func NewPerson(firstName, lastName string, age, weight int) *Person {
+	return &Person{
+		firstname: firstName,
+		lastName:  lastName,
+		age:       age,
+		weight:    weight,
+	}
+}
+
+func (p *Person) sayHello() {
+	message := fmt.Sprintf("Hello there %s, welcome to the team", p.firstname)
+	fmt.Println(message)
+}
+
+// OH MY GOD, golang is so wonderful!!!!
 func main() {
-	fmt.Println("Lungu boy")
+	emeka := NewPerson("Nnaemeka", "Onyeokoro", 22, 200)
+	emeka.sayHello()
 }
